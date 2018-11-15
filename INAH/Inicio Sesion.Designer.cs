df1ec7abecabea894779d49376e0d53e847d6d70
SoloMissael <missael_lopez@hotmail.com>
2018-11-14 23:12:50 -0700

    Commit de prueba

--- INAH/Inicio Sesion.Designer.cs
+++ INAH/Inicio Sesion.Designer.cs
@@ -83,7 +83,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(99, 227);
+            this.button1.Location = new System.Drawing.Point(99, 223);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 23);
             this.button1.TabIndex = 5;
@@ -93,7 +93,7 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(84, 207);
+            this.linkLabel1.Location = new System.Drawing.Point(96, 207);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(125, 13);
             this.linkLabel1.TabIndex = 6;
